1d84c78494f09921981a9640bf5720b77b5ced7b
iamsarb001 <sarbbsandhu555@gmail.com>
2025-03-02 13:02:45 +0530

    checking ci- pipeline

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -2,6 +2,7 @@
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 
+console.log('main section ')
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
         <SessionProvider>
